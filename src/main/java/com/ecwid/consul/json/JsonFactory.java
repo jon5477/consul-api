f4756fe13512e189c1ca10bf0f2a670a5e1905e1
Jon Huang <jonh5477@gmail.com>
2024-02-05 19:50:21 -0800

    Add automatic module name and ignore unknown properties in JSON.

--- src/main/java/com/ecwid/consul/json/JsonFactory.java
+++ src/main/java/com/ecwid/consul/json/JsonFactory.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
@@ -16,6 +17,7 @@ import com.google.gson.Gson;
 public final class JsonFactory {
 	private static final String SERIALIZE_ERROR = "Unable to serialize object from JSON";
 	private static final String SER_TYPE_ERROR = "Unhandled JSON serialization type: ";
+
 	/**
 	 * The JSON serializer types that is used to serialize POJOs to JSON.
 	 */
@@ -35,7 +37,9 @@ public final class JsonFactory {
 		if (typeRef.get() == null) {
 			try {
 				Class<?> jacksonClass = Class.forName("com.fasterxml.jackson.databind.ObjectMapper");
-				objMapperRef.set((ObjectMapper) jacksonClass.getDeclaredConstructor().newInstance());
+				ObjectMapper objMapper = (ObjectMapper) jacksonClass.getDeclaredConstructor().newInstance();
+				objMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+				objMapperRef.set(objMapper);
 				typeRef.set(JsonSerializerType.JACKSON);
 				return;
 			} catch (ClassNotFoundException e) {
